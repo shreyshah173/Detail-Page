e2e4c1a883fb5c6bda4d1a9452398d95d1201def
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-03-03 16:19:41 +0530

    popup debugging

--- javascript/popup.js
+++ javascript/popup.js
@@ -1,9 +1,11 @@
+let showDetailsPopUp;
+let goBack;
+
 document.addEventListener("DOMContentLoaded", function () {
 	const body = document.querySelector("body");
 	const overlay = document.querySelector(".overlay");
 
-  const showDetailsPopUp = () => {
-    
+	showDetailsPopUp = () => {
 		const moreSection = document.querySelector(".more_section");
 
 		if (moreSection.style.left == "0px") {
@@ -17,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	};
 
-	const goBack = () => {
+	goBack = () => {
 		const moreSection2 = document.querySelector(".more_section2");
 
 		if (moreSection2.style.left == "0px") {
