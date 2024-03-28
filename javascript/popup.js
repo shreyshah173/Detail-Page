626d483cd58f18e82bb6eab8d34f485ef8b3c63b
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-03-28 15:59:03 +0530

    more external and footer section

--- javascript/popup.js
+++ javascript/popup.js
@@ -4,6 +4,7 @@ let goBack;
 document.addEventListener("DOMContentLoaded", function () {
 	const body = document.querySelector("body");
 	const overlay = document.querySelector(".overlay");
+	const footer = document.querySelector(".more-footer-container");
 
 	showDetailsPopUp = () => {
 		const moreSection = document.querySelector(".more_section");
@@ -12,10 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
 			moreSection.style.left = "-100%";
 			body.style.overflow = "auto";
 			overlay.style.display = "none";
+			footer.style.maxHeight = "0px"
 		} else {
 			moreSection.style.left = "0px";
 			body.style.overflow = "hidden";
 			overlay.style.display = "block";
+			footer.style.maxHeight = "100px";
 		}
 	};
 
